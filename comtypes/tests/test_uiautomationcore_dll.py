aba16aed5fd09964cbf715fc3d6461181977b5d5
junkmd <jun.k.job.finding12@gmail.com>
2022-04-17 15:47:16 +0900

    update uia test

--- comtypes/tests/test_uiautomationcore_dll.py
+++ comtypes/tests/test_uiautomationcore_dll.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
-import gc
-from typing import List
+import os
+import subprocess as sp
+import time
+from typing import Any, List
 
 import comtypes
 from comtypes.client import CreateObject, GetEvents, GetModule
@@ -17,10 +19,30 @@ def _setup_module(cleanup_gen_import):
 class Test_IUIAutomation:
 	def test_CoCreateInstanceIUIAutomation(self):
 		from comtypes.gen import UIAutomationClient as uia_core
+
+		def FindAllChildren(
+			elm: uia_core.IUIAutomationElement,
+			cond: uia_core.IUIAutomationCondition,
+		) -> List[uia_core.IUIAutomationElement]:
+			found = elm.FindAll(uia_core.TreeScope_Children, cond)
+			return [found.GetElement(i) for i in range(0, found.Length)]
+
 		iuia = comtypes.CoCreateInstance(
             uia_core.CUIAutomation().IPersist_GetClassID(),
             interface=uia_core.IUIAutomation,
             clsctx=comtypes.CLSCTX_INPROC_SERVER
         )
-		true_cond = iuia.CreateTrueCondition()
-		root = iuia.GetRootElement()
+		popen = sp.Popen("notepad")
+		found = []
+		while not found:
+			cond = iuia.CreateTrueCondition()
+			root = iuia.GetRootElement()
+			found = [
+				elm for elm in FindAllChildren(root, cond)
+				if elm.CurrentProcessId == popen.pid
+			]
+		wnd = found[0]
+		assert wnd.CurrentClassName == "Notepad"
+		ptn = wnd.GetCurrentPattern(uia_core.UIA_WindowPatternId)
+		iface = ptn.QueryInterface(uia_core.IUIAutomationWindowPattern)
+		iface.Close()
