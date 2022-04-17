c438b26e595c2748641531bdf2767cd4256fd8b1
junkmd <jun.k.job.finding12@gmail.com>
2022-04-17 19:37:39 +0900

    update test

--- comtypes/tests/test_excel.py
+++ comtypes/tests/test_excel.py
@@ -3,7 +3,7 @@ from comtypes.client import CreateObject
 import pytest
 
 
-# @pytest.mark.skip(reason="WORKAROUND! Excel is not installed on Environment.")
+@pytest.mark.skip(reason="WORKAROUND! Excel is not installed on Environment.")
 class Test_Excel_Application:
 	def test_CreateObject(self):
 		xlapp = CreateObject("Excel.Application")
